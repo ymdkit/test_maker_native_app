ae44ea41a633dd38610169d005e91ac9399c29c3
ymdkit <banira0428@gmail.com>
2023-02-21 16:25:55 +0900

    正誤判定処理の修正

--- lib/usecase/check_is_correct_use_case.dart
+++ lib/usecase/check_is_correct_use_case.dart
@@ -1,19 +1,49 @@
 import 'package:flutter/foundation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:test_maker_native_app/model/question.dart';
+import 'package:test_maker_native_app/ui/utils/shared_preference.dart';
 
-final checkIsCorrectUseCaseProvider =
-    Provider((ref) => CheckIsCorrectUseCase());
+final checkIsCorrectUseCaseProvider = Provider.autoDispose(
+  (ref) => CheckIsCorrectUseCase(
+    isCaseInsensitive: ref.watch(
+      sharedPreferencesProvider.select((value) =>
+          value.getBool(PreferenceKey.isCaseInsensitive.name) ?? false),
+    ),
+  ),
+);
 
 class CheckIsCorrectUseCase {
+  const CheckIsCorrectUseCase({
+    required this.isCaseInsensitive,
+  });
+  // TODO: テスト書く
+
+  final bool isCaseInsensitive;
+
   bool call({
     required Question question,
     required List<String> attemptAnswers,
   }) {
+    final originalAnswers = question.answers;
+
     if (question.isCheckAnswerOrder) {
-      return listEquals(question.answers, attemptAnswers);
+      if (isCaseInsensitive) {
+        return listEquals(
+          originalAnswers.map((e) => e.toLowerCase()).toList(),
+          attemptAnswers.map((e) => e.toLowerCase()).toList(),
+        );
+      } else {
+        return listEquals(originalAnswers, attemptAnswers);
+      }
     } else {
-      return setEquals(question.answers.toSet(), attemptAnswers.toSet());
+      if (isCaseInsensitive) {
+        return setEquals(
+          originalAnswers.map((e) => e.toLowerCase()).toSet(),
+          attemptAnswers.map((e) => e.toLowerCase()).toSet(),
+        );
+      } else {
+        return setEquals(originalAnswers.toSet(), attemptAnswers.toSet());
+      }
     }
   }
 }
