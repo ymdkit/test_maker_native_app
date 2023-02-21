0c9e870109987fc9f92d914475884902ae4c91ec
ymdkit <banira0428@gmail.com>
2023-02-21 20:02:45 +0900

    ディレクトリ構成の整理

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:auto_route/empty_router_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:test_maker_native_app/feature/setting/ui/debug_page.dart';
+import 'package:test_maker_native_app/feature/setting/ui/setting_page.dart';
+import 'package:test_maker_native_app/feature/setting/ui/trash_page.dart';
 import 'package:test_maker_native_app/main.dart';
 import 'package:test_maker_native_app/model/folder.dart';
 import 'package:test_maker_native_app/model/question.dart';
@@ -15,9 +18,6 @@ import 'package:test_maker_native_app/ui/page/question/create_question_page.dart
 import 'package:test_maker_native_app/ui/page/question/edit_question_page.dart';
 import 'package:test_maker_native_app/ui/page/record/record_page.dart';
 import 'package:test_maker_native_app/ui/page/search/search_workbook_page.dart';
-import 'package:test_maker_native_app/ui/page/setting/debug_page.dart';
-import 'package:test_maker_native_app/ui/page/setting/setting_page.dart';
-import 'package:test_maker_native_app/ui/page/setting/trash_page.dart';
 import 'package:test_maker_native_app/ui/page/workbook/answer_workbook_page.dart';
 import 'package:test_maker_native_app/ui/page/workbook/answer_workbook_result_page.dart';
 import 'package:test_maker_native_app/ui/page/workbook/create_workbook_page.dart';
