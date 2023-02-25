08d8dda9e3a2b57a50e415afd757c8526bd1f9dc
ymdkit <banira0428@gmail.com>
2023-02-25 15:54:53 +0900

    DynamicLinks のセットアップ

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -10,11 +11,14 @@ import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:package_info/package_info.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:test_maker_native_app/constants/app_data_location.dart';
 import 'package:test_maker_native_app/feature/setting/state/preferences_state.dart';
 import 'package:test_maker_native_app/feature/setting/utils/shared_preference.dart';
+import 'package:test_maker_native_app/feature/workbook/repository/workbook_repository.dart';
 import 'package:test_maker_native_app/router/app_router.dart';
 import 'package:test_maker_native_app/theme/theme_ext.dart';
 import 'package:test_maker_native_app/utils/package_information.dart';
+import 'package:test_maker_native_app/widget/app_snack_bar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,6 +29,8 @@ void main() async {
     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
     return true;
   };
+
+  final initialLink = await FirebaseDynamicLinks.instance.getInitialLink();
 
   await MobileAds.instance.initialize();
   await MobileAds.instance.updateRequestConfiguration(
@@ -44,19 +50,38 @@ void main() async {
         packageInfoProvider.overrideWithValue(packageInfo),
         sharedPreferencesProvider.overrideWithValue(preferences),
       ],
-      child: const MyApp(),
+      child: MyApp(
+        initialLink: initialLink,
+      ),
     ),
   );
 }
 
 class MyApp extends HookConsumerWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+    required this.initialLink,
+  });
+
+  final PendingDynamicLinkData? initialLink;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final appRouter = useMemoized(() => AppRouter());
     final appThemeColor =
         ref.watch(preferencesStateProvider.select((value) => value.themeColor));
+
+    useEffect(() {
+      FirebaseDynamicLinks.instance.onLink.listen((event) async {
+        await _handleDynamicLink(event, ref, context, appRouter);
+      });
+      () async {
+        if (initialLink != null) {
+          await _handleDynamicLink(initialLink!, ref, context, appRouter);
+        }
+      }();
+      return null;
+    }, const []);
 
     return MaterialApp.router(
       // TODO(ymdkit): Dynamic Links のハンドリング
@@ -99,6 +124,29 @@ class MyApp extends HookConsumerWidget {
       ),
       routeInformationParser: appRouter.defaultRouteParser(),
     );
+  }
+
+  Future<void> _handleDynamicLink(PendingDynamicLinkData event, WidgetRef ref,
+      BuildContext context, AppRouter appRouter) async {
+    final link = event.link.toString();
+    final regex = RegExp(
+        r'(?<=https://testmaker-1cb29\.com/).*|(?<=https://ankimaker\.com/).*');
+    final match = regex.stringMatch(link);
+
+    //TODO: グループ用の招待リンク受け取り
+
+    if (match != null) {
+      final workbookRepository =
+          ref.read(workbookRepositoryProvider(AppDataLocation.remoteOwned));
+      final result = await workbookRepository.getWorkbook(workbookId: match);
+
+      result.match(
+        (l) => showAppSnackBar(context, l.message),
+        (r) => appRouter.push(
+          AnswerWorkbookRoute(workbook: r),
+        ),
+      );
+    }
   }
 }
 
