2cd3641ed2eb32a32f419449858881c157646058
ymdkit <banira0428@gmail.com>
2023-03-05 13:09:38 +0900

    解答履歴をグラフで表示できるようにする

--- lib/constants/magic_number.dart
+++ lib/constants/magic_number.dart
@@ -1,4 +1,6 @@
 class MagicNumber {
   static const int requireAnswerCountForAppTrackingTransparency = 1;
   static const int requireAnswerCountForInAppReview = 5;
+
+  static const int showRecordDateCount = 7;
 }
--- lib/feature/record/ui/record_page.dart
+++ lib/feature/record/ui/record_page.dart
@@ -1,9 +1,15 @@
+import 'package:dartx/dartx.dart';
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:test_maker_native_app/constants/magic_number.dart';
 import 'package:test_maker_native_app/feature/record/state/answer_histories_state.dart';
 import 'package:test_maker_native_app/widget/app_ad_widget.dart';
 import 'package:test_maker_native_app/widget/app_ad_wrapper.dart';
 import 'package:test_maker_native_app/widget/app_error_content.dart';
+import 'package:test_maker_native_app/widget/app_sliver_space.dart';
 
 class RecordPage extends HookConsumerWidget {
   const RecordPage({super.key});
@@ -20,16 +26,99 @@ class RecordPage extends HookConsumerWidget {
         body: answerHistories.when(
           data: (answerHistories) => RefreshIndicator(
             onRefresh: () async => ref.refresh(answerHistoriesProvider),
-            child: ListView.builder(
-              itemCount: answerHistories.length,
-              itemBuilder: (context, index) {
-                final answerHistory = answerHistories[index];
-                return ListTile(
-                  title: Text(answerHistory.questionId),
-                  subtitle: Text(answerHistory.answerHistoryId),
-                  trailing: Text(answerHistory.isCorrect ? '正解' : '不正解'),
-                );
-              },
+            child: CustomScrollView(
+              slivers: [
+                const AppSliverSpace(height: 16),
+                SliverPadding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  sliver: SliverToBoxAdapter(
+                    child: SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.25,
+                      child: BarChart(
+                        BarChartData(
+                          alignment: BarChartAlignment.spaceAround,
+                          barTouchData: BarTouchData(
+                            enabled: false,
+                          ),
+                          titlesData: FlTitlesData(
+                            rightTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
+                            topTitles: AxisTitles(
+                              sideTitles: SideTitles(showTitles: false),
+                            ),
+                            bottomTitles: AxisTitles(
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                getTitlesWidget: bottomTitles,
+                                reservedSize: 36,
+                              ),
+                            ),
+                          ),
+                          borderData: FlBorderData(
+                            show: false,
+                          ),
+                          gridData: FlGridData(
+                            show: false,
+                          ),
+                          barGroups: List.generate(
+                              MagicNumber.showRecordDateCount, (index) {
+                            final date = DateTime.now()
+                                .subtract(
+                                  Duration(
+                                    days: MagicNumber.showRecordDateCount -
+                                        1 -
+                                        index,
+                                  ),
+                                )
+                                .date;
+                            return BarChartGroupData(
+                              x: index,
+                              barRods: [
+                                BarChartRodData(
+                                  toY: answerHistories
+                                      .where((answerHistory) =>
+                                          !answerHistory.isCorrect &&
+                                          answerHistory.createdAt.date
+                                              .eqvYearMonthDay(date))
+                                      .length
+                                      .toDouble(),
+                                  color: Colors.grey,
+                                ),
+                                BarChartRodData(
+                                  toY: answerHistories
+                                      .where(
+                                        (answerHistory) =>
+                                            answerHistory.isCorrect &&
+                                            answerHistory.createdAt.date
+                                                .eqvYearMonthDay(date),
+                                      )
+                                      .length
+                                      .toDouble(),
+                                ),
+                              ],
+                            );
+                          }),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                const AppSliverSpace(height: 16),
+                SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      final answerHistory = answerHistories[index];
+                      return ListTile(
+                        title: Text(answerHistory.questionId),
+                        subtitle: Text(answerHistory.answerHistoryId),
+                        trailing: Text(answerHistory.isCorrect ? '正解' : '不正解'),
+                      );
+                    },
+                    childCount: answerHistories.length,
+                  ),
+                ),
+              ],
             ),
           ),
           error: (error, stackTrace) => Center(
@@ -39,6 +128,26 @@ class RecordPage extends HookConsumerWidget {
             child: CircularProgressIndicator(),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget bottomTitles(double value, TitleMeta meta) {
+    const limit = MagicNumber.showRecordDateCount;
+    final titles = List.generate(
+      limit,
+      (index) => DateFormat('MM/dd').format(
+        DateTime.now().subtract(
+          Duration(days: limit - 1 - index),
+        ),
+      ),
+    );
+
+    return SideTitleWidget(
+      axisSide: meta.axisSide,
+      space: 16, //margin top
+      child: Text(
+        titles[value.toInt()],
       ),
     );
   }
